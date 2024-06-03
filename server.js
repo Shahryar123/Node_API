fd5c82ba4143980af0e054ad51679558daca4c8c
Shahryar <muhammad.shahryar@braincrop.net>
2024-06-03 15:53:15 +0500

    Feat:"Connect MongoDB with Node.Js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const mongoose = require("mongoose")
 
 const app = express()
 
@@ -6,6 +7,18 @@ const app = express()
 app.get("/",(req,res)=>{
     res.send("Hello this is server")
 })
-app.listen(3000 , ()=>{
-    console.log("Hello this is console")
+app.get("/blog",(req,res)=>{
+    res.send("Hello this is blog")
 })
+
+mongoose.set('strictQuery', false);
+mongoose.connect('mongodb://127.0.0.1:27017/NodeRestApi')
+    .then(() => {
+        console.log('Connection to MongoDB successful');
+        app.listen(3000 , ()=>{
+            console.log("Hello this is console")
+        })
+    })
+    .catch((err) => {
+        console.error('Error connecting to MongoDB', err);
+    });
