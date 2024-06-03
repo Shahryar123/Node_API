6bfa20790ab80090a344860845bbd0bd6c3c14e8
Shahryar <muhammad.shahryar@braincrop.net>
2024-06-03 13:24:50 +0500

    initial commit

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log('Hello World'); 
+const express = require("express")
+
+const app = express()
+
+
+app.get("/",(req,res)=>{
+    res.send("Hello this is server")
+})
+app.listen(3000 , ()=>{
+    console.log("Hello this is console")
+})
